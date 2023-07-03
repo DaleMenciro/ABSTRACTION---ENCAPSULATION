8b9e371c098bb16522a899996c5e4087e1d6a9ef
Dale Menciro <dale_menciro@protonmail.com>
2023-07-04 03:11:49 +0800

    pseudocodes for attributes and methods

--- pet/Pet.py
+++ pet/Pet.py
@@ -1,1 +1,19 @@
 class Pet: #class representing pet
+
+    '''
+    Attributes:
+    __name (for the name of a pet)
+    __animal_type (for the type of animal that a pet is. Example values are ‘Dog’, ‘Cat’, and ‘Bird’)
+    __age (for the pet’s age)
+
+    Methods:
+        set_name() - ssigns a value to the __name field.
+        set_animal_type()- assigns a value to the __animal_type field
+        set_age() - assigns a value to the __age field.
+        get_name() - returns the value of the __name field
+        get_animal_type() - returns the value of the __animal_type field
+        get_age() - returns the value of the __age field
+        
+    '''
+    
+
