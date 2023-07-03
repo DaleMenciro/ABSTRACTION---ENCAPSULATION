135a9bf659a4d2628c63a2d89abf3324cd57efb2
Dale Menciro <dale_menciro@protonmail.com>
2023-07-04 00:59:06 +0800

    initializing GUICar

--- Car_GUI.py
+++ Car_GUI.py
@@ -1,2 +1,7 @@
 import tkinter as tk
 
+class GUICar:
+    #initializing GUICar object with master window and car object
+    def __init__(self,master,car):
+        self.master = master
+        self.car = car
