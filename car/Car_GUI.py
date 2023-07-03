a3f3fa53dc3d72417f0ddc14e3cae1580724cb06
Dale Menciro <dale_menciro@protonmail.com>
2023-07-04 01:45:13 +0800

    acceleration button

--- car/Car_GUI.py
+++ car/Car_GUI.py
@@ -20,8 +20,20 @@ class GUICar:
         #frame for buttons
         self.button_frames = tk.Frame(self.master,bg="#f0f0f0")
         self.button_frames.pack()
-        
+
         #acceleration button
+        self.acceleration_button = tk.Button(
+            self.button_frame,
+            text="Accelerate"
+            font= ("Arial", 12)
+            bg = "#66bb6a",
+            fg = "white"
+            activebackground="#4caf50",
+            activeforeground= "white"
+            command= self.accelerate,
+        )
+        self.acceleration_button.grid(row = 0, column= 0, padx = 0)
+        
         #brake button
 
 root = tk.Tk()
