50f3b4548dbe4050bc41ed07d32f15768449e03a
Dale Menciro <dale_menciro@protonmail.com>
2023-07-04 01:16:39 +0800

    moving Car_GUI.py to folder

--- car/Car_GUI.py
+++ car/Car_GUI.py
@@ -2,10 +2,11 @@ import tkinter as tk
 
 class GUICar:
     #initializing GUICar object with master window and car object
-    def __init__(self,master,car):
+    def __init__(self, master, car):
         self.master = master
         self.car = car
 
         self.master.title ("Car GUI")
         self.master.geometry ("300x200")
         self.master.config(bg="#f0f0f0") #background color
+
