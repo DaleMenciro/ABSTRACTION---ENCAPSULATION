e258899544a889c4849395d661bda58c6e83928d
Dale Menciro <dale_menciro@protonmail.com>
2023-07-03 23:35:32 +0800

    input attributes and methods for guide

--- car/Car.py
+++ car/Car.py
@@ -2,4 +2,16 @@ class Car:
     '''
      a class to represent car objects
     
+    Class Attributes:
+    
+    __year_model (for the car’s year model)
+    __make (for the make of the car)
+    __speed (for the car’s current speed)
+
+    Methods:
+    
+    accelerate()
+    brake()
+    get_speed()
+    
     '''
