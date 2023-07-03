e452f39c07c88cc3b286938f5a23c633bf83d5ff
Dale Menciro <dale_menciro@protonmail.com>
2023-07-04 04:21:20 +0800

    setting up window

--- pet/Pet_GUI.py
+++ pet/Pet_GUI.py
@@ -5,4 +5,7 @@ class PetGUI:
     def __init__(self, master, pet):
         self.master = master
         self.pet = pet
-        
+
+        self.master.title("Pet GUI")
+        self.master.geometry("300x200")
+        self.master.config(bg="#f0f0f0")
