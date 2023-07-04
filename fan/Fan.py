286bfb918485410a08bdf762fc565d0e4b0d1c28
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 02:17:37 +0800

    adding pseudocodes for attributes and methods for guidance

--- fan/Fan.py
+++ fan/Fan.py
@@ -1,1 +1,18 @@
 class Fan:
+    '''
+    Attributes:
+    __speed : int - fan's speed
+    __on : bool - specifies whether the fan is on or off (the default is False)
+    __radius : float - radius of fan
+    __color : str - color of the fan
+
+    Methods:
+     get_speed - gets fan's speed
+     set_speed - sets fan's speed
+     is_on - returns the current state of the fan (on/off).
+     set_on - set the electric fan whenever it is on/off
+     get_radius - gets fan's radius
+     set_radius - sets fan's radiues
+     get_color - gets fan's color
+     set_color - sets fan's color
+    '''
