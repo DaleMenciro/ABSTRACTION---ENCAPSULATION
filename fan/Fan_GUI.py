11c955568295a27296c72330981f3ddd303bcdef
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 03:29:22 +0800

    parameters on GUI elements

--- fan/Fan_GUI.py
+++ fan/Fan_GUI.py
@@ -6,7 +6,11 @@ class FanGUI:
         self.parent = parent
 
         #GUI elements
+        self.speed_scale = tk.Scale(self.parent, from_=1, to=3, orient=tk.HORIZONTAL, label="Speed")
+        self.radius_scale = tk.Scale(self.parent, from_=1, to=10, orient=tk.HORIZONTAL, label="Radius")
+        self.color_btn = tk.Button(self.parent, text="Select Color")
+        self.on_off_btn = tk.Button(self.parent, text="Turn On/Off")
+        self.status_label = tk.Label(self.parent, text="Fan Status: ")
 
         #Elements layout
 
-        
