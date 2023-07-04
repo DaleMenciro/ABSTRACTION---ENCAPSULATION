15d9457c70a32974a9f646bef5bf8124bca7c798
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 02:44:17 +0800

    setting parameters for the methods

--- fan/Fan.py
+++ fan/Fan.py
@@ -21,7 +21,7 @@ class Fan:
     FAST = 3
 
     def __init__(self, speed=SLOW, radius= 5, color= 'blue', on=False):
-         """
+        """
         Initializes a Fan object with the specified properties.
 
         Arguments:
@@ -36,19 +36,27 @@ class Fan:
         self.__color = color
     
     def get_speed(self):
+        return self.__speed
     
     def set_speed(self, speed):
+        self.__speed = speed
     
     def is_on(self):
+        return self.__on
     
     def set_on(self, on):
+        self.__on = on
     
     def get_radius(self):
+        return self.__radius
     
     def set_radius(self, radius):
+        self.__radius = radius
     
     def get_color(self):
+        return self.__color
 
     def set_color(self, color):
+        self.__color = color
 
         
