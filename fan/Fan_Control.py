579fa8843566f41d29597b07cf874ee8247afeaa
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 12:56:47 +0800

    pseudocodes for class

--- fan/Fan_Control.py
+++ fan/Fan_Control.py
@@ -2,4 +2,8 @@ class FanControl:
     def __init__(self, fan_gui):
         self.fan_gui = fan_gui
 
-        
+        #Create Fan object
+
+        #Set initial value
+
+        #Set event handlers
