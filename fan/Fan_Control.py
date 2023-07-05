0b78dab04ce299e44ed64163786f1f385c00d37c
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 13:00:02 +0800

    create fan object and setting initial value

--- fan/Fan_Control.py
+++ fan/Fan_Control.py
@@ -3,7 +3,12 @@ class FanControl:
         self.fan_gui = fan_gui
 
         #Create Fan object
+        self.fan = Fan()
 
         #Set initial value
+        self.fan_view.set_speed(self.fan.get_speed())
+        self.fan_view.set_radius(self.fan.get_radius())
+        self.fan_view.set_color(self.fan.get_color())
+        self.fan_view.set_on(self.fan.is_on())
 
         #Set event handlers
