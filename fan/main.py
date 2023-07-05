19e2597fae8f054dd514a254242b68ce8d5a684c
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 15:40:52 +0800

    parameters for center_window

--- fan/main.py
+++ fan/main.py
@@ -39,3 +39,9 @@ class FanTest:
         window2.mainloop()
     
     def center_window(self, window):
+        window.update_idletasks()
+        width = window.winfo_width()
+        height = window.winfo_height()
+        x = (window.winfo_screenwidth() // 2) - (width // 2)
+        y = (window.winfo_screenheight() // 2) - (height // 2)
+        window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
