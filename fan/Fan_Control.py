4e400f6c9a4b8b1601da2dc77a49da19ff779d75
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 13:04:47 +0800

    setting event handlers

--- fan/Fan_Control.py
+++ fan/Fan_Control.py
@@ -12,3 +12,7 @@ class FanControl:
         self.fan_view.set_on(self.fan.is_on())
 
         #Set event handlers
+        self.fan_view.speed_scale.config(command=self.change_speed)
+        self.fan_view.radius_scale.config(command=self.change_radius)
+        self.fan_view.color_btn.config(command=self.change_color)
+        self.fan_view.on_off_btn.config(command=self.toggle_fan)
