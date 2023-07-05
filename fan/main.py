40714d85f379d270f05bfa8812d680d9acb98fa2
Dale Menciro <dale_menciro@protonmail.com>
2023-07-05 13:14:06 +0800

    creating 2 fan objects

--- fan/main.py
+++ fan/main.py
@@ -5,3 +5,7 @@ from Fan import Fan
 
 class FanTest:
     def __init__(self):
+        #create two Fan objects with different properties
+        self.fan1 = Fan(Fan.FAST, 10, 'yellow', True)
+        self.fan2 = Fan(Fan.MEDIUM, 5, 'blue', False)
+    
